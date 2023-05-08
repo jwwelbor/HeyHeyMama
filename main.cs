e948a121e4a8f649962c95148e95282d48f2a169
jwwelbor <jwwelbor@gmail.com>
2023-05-07 20:47:02 -0500

    Create main.cs
    rough draft to randomize a playlist

---
+++ main.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+
+namespace SpotifyPlaylistRandomizer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var clientId = "your_client_id";
+            var clientSecret = "your_client_secret";
+            var redirectUri = "http://localhost:8888/callback";
+
+            var auth = new AuthorizationCodeAuth(
+                clientId,
+                clientSecret,
+                redirectUri,
+                new[] { Scopes.PlaylistModifyPublic }
+            );
+
+            auth.AuthReceived += async (sender, payload) =>
+            {
+                auth.Stop();
+                var token = await auth.ExchangeCode(payload.Code);
+                var api = new SpotifyWebAPI()
+                {
+                    AccessToken = token.AccessToken,
+                    TokenType = token.TokenType
+                };
+
+                var playlistId = "your_playlist_id";
+                var playlist = await api.GetPlaylistTracks(playlistId);
+
+                var random = new Random();
+                var randomizedTracks = playlist.Tracks.Items.OrderBy(x => random.Next());
+
+                await api.ReorderPlaylistTracks(
+                    playlistId,
+                    new ReorderPlaylistTracksRequest()
+                    {
+                        RangeStart = 0,
+                        RangeLength = playlist.Tracks.Total,
+                        InsertBefore = 0,
+                        SnapshotId = playlist.SnapshotId,
+                        TrackUris = randomizedTracks.Select(x => x.Track.Uri).ToList()
+                    }
+                );
+            };
+
+            auth.Start();
+            auth.OpenBrowser();
+            Console.ReadLine();
+        }
+    }
+}
